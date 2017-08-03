c4dcaccc9613ba0389ffb0b751f0cbdfa03ba131
HarishAtGitHub <harish.pmprk@gmail.com>
2017-08-03 12:08:41 -0700

    adding the project to github

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # URL-To-Image-Converter-AWS-LAMBDA
+
+This project takes any URL and takes the screenshot of the webpage and loads into s3 bucket and gives it as result.
+
+This can be used in scenarios where you need to display an image as result from alexa in the alexa cards app.
+
+This is an AWS Lambda deployment package.
+
+PREREQ:
+======
+S3- BUCKET NAME: alexa-image-store
+BUCKET-SUBFOLDER-NAME: alexa-location-annotator
+
+API-DOCS:
+========
+REQUEST BODY
+{
+  "url" : "https://myatlascms.maps.asu.edu/map/?id=120&s=p&reference=MU#!sbc/",
+  "max_page_load_time" : 4,
+  "window_size_width" (OPTIONAL) : 1024,
+  "window_size_height" (OPTIONAL) : 768
+}
+
+
+
+How is packaging done ?
+=======================
+
+phantomjs/
+selenium/
+selenium-3.4.3.dist-info/
+lambda_function.py
+
+
+
---
+++ lambda_function.py
@@ -0,0 +1,52 @@
+def lambda_handler(event, context):
+    from selenium import webdriver
+    from selenium.common.exceptions import TimeoutException
+
+    # input
+    url = event['url']
+
+    # constants 
+    try:
+        IMAGE_NAME = event['image_name']
+    except KeyError as ke:
+        IMAGE_NAME = 'img.png'
+    MAX_PAGE_LOAD_TIME = 4
+    WINDOW_SIZE_WIDTH = 1024
+    WINDOW_SIZE_HEIGHT = 768
+
+    # function
+    import os
+    driver = webdriver.PhantomJS(service_log_path=os.path.devnull, executable_path="./phantomjs/bin/phantomjs")
+    driver.set_window_size(WINDOW_SIZE_WIDTH, WINDOW_SIZE_HEIGHT) # set the window size that you need 
+    driver.get(url)
+    load_page_completely(MAX_PAGE_LOAD_TIME)
+    image = driver.get_screenshot_as_base64()
+    import base64
+    url = load_image_to_s3(base64.b64decode(image), IMAGE_NAME)
+    return {"imageurl" : url}
+
+def load_page_completely(page_load_time):
+    import time
+    time.sleep(page_load_time)
+
+def load_image_to_s3(image, name):
+    import boto3
+    bucket_name = "alexa-image-store"
+    folder_name = "alexa-location-annotator" # can be used in future
+    object_name = name # for now 
+    key = folder_name + '/' + object_name
+    s3_client = boto3.client('s3')
+    s3_client.put_object(ACL='public-read', 
+                         Body=image, 
+                         Key=key, 
+                         Bucket=bucket_name,
+                         Metadata={"Content-Type" :"image/png"},
+                         ContentEncoding="base64")
+    url = '{}/{}/{}'.format(s3_client.meta.endpoint_url, bucket_name, key)
+    return url
+# run
+#S event = { "url" : 'https://myatlascms.maps.asu.edu/map/?id=120&s=p&reference=MU#!sbc/' }
+#S lambda_handler(event, "context")
+#event = { "url" : 'https://myatlascms.maps.asu.edu/map/?id=120&s=p&reference=MU#!sbc/' }
+#lambda_handler(event, "context")
+
